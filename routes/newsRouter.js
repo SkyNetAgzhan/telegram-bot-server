1781f68502201c7cc603aa46538cc3299a09d3ec
Shauyenov_A <akzhan133@gmail.com>
2025-02-13 18:13:23 +0500

    lupdate

--- routes/newsRouter.js
+++ routes/newsRouter.js
@@ -3,9 +3,8 @@ const router = new Router();
 const NewsController = require('../controllers/newsController');
 const checkRole = require('../middleware/checkRoleMiddleware.js');
 const newsController = require('../controllers/newsController');
-const authMiddleware = require('../middleware/authMiddleware.js');
 
-router.post('/create', NewsController.create);
+router.post('/create', checkRole('ADMIN'), NewsController.create);
 router.get('/', newsController.getAll);
 router.get('/:id', newsController.getOne);
 router.put('/update/:id', checkRole('ADMIN'), newsController.update);
